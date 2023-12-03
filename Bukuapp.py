583971ef61feb22a4b483fd283058231e8402426
Nadia Yasmin <130304989+nadyasmin@users.noreply.github.com>
2023-12-03 18:40:16 +0700

    Update Bukuapp.py

--- Bukuapp.py
+++ Bukuapp.py
@@ -67,8 +67,8 @@ if page == "Edit Data":
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
                             query = text('UPDATE buku \
-                                            SET Kode Buku=:1, Judul Buku=:2, Genre=:3, Tahun Terbit=:4, \
-                                          Pengarang=:5, Penerbit=:6, Kode Rak=:7, Status=:8, Gambar Buku=:9 \
+                                            SET "Kode Buku"=:1, "Judul Buku"=:2, "Genre"=:3, "Tahun Terbit"=:4, \
+                                          "Pengarang"=:5, "Penerbit"=:6, "Kode Rak"=:7, "Status"=:8, "Gambar Buku"=:9 \
                                           WHERE id=:10;')
                             session.execute(query, {'1':code_baru, '2':title_baru, '3':genre_baru, '4':year_baru, '5':author_baru, 
                                                     '6':publisher_baru, '7':rack_baru, '8':status_baru, '9':pict_baru, '10':id})
