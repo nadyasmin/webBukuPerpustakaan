a33dac83f2b8e387ad2133b0e29f29dd761f7807
Nadia Yasmin <130304989+nadyasmin@users.noreply.github.com>
2023-12-03 17:31:04 +0700

    Update Bukuapp.py

--- Bukuapp.py
+++ Bukuapp.py
@@ -14,7 +14,7 @@ st.header('DATABASE BUKU PERPUSTAKAAN')
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
 
 if page == "View Data":
-    data = conn.query('SELECT * FROM buku ORDER By title;', ttl="0").set_index('title')
+    data = conn.query('SELECT * FROM buku ORDER By title;').set_index('title')
     st.dataframe(data)
 
 if page == "Edit Data":
