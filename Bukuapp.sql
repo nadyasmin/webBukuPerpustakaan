25940420490b1de3aa2296821a8d6872bf11c848
nurfjry <129614413+nurfjry@users.noreply.github.com>
2023-12-06 13:25:19 +0700

    Update Bukuapp.sql

--- Bukuapp.sql
+++ Bukuapp.sql
@@ -16,7 +16,7 @@ values
 	('ROM001', 'Matahari Terbenam di Pantai', 'Romantis', '2018', 'Aulia Nuryani', 'Cinta Sejati Publishing', 'N001', 'Tersedia')
 	('ROM002', 'Dikta & Hukum', 'Romantis', '2021', 'Dhian Farah', 'ASOKA AKSARA X LOVEABLE', 'N001', 'Tersedia')
 	('ROM003', 'Dilan 1990', 'Romantis', '2015', 'Pidi Baiq', 'DAR! Mizan', 'N001', 'Dipinjam')
-	('AKS001', 'Tiga Sandera Terakhir', '2015', 'Brahmanto Anindito', 'Noura Publishing', 'N002', 'Tersedia')
-	('AKS002', 'Seteru 1 Guru', '2015', 'Haris Priyatna', 'Bandung Qanita', 'N002', 'Dipinjam')
+	('AKS001', 'Tiga Sandera Terakhir', 'Aksi', '2015', 'Brahmanto Anindito', 'Noura Publishing', 'N002', 'Tersedia')
+	('AKS002', 'Seteru 1 Guru', 'Aksi', '2015', 'Haris Priyatna', 'Bandung Qanita', 'N002', 'Dipinjam')
 	;
 	
